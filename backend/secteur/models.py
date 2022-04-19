c500f1ff0ac62fe425193ed563be717f04656df6
Youcef <chertouhjugurtha@gmail.com>
2022-04-19 13:48:57 +0100

    Added new tables secteur stable

--- backend/secteur/models.py
+++ backend/secteur/models.py
@@ -12,7 +12,6 @@ class Secteur(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     secteur = models.CharField(unique=True, max_length=255, null=False, blank=False)
     branche = models.ForeignKey(Branche, on_delete=models.SET_NULL, null=True)
-    branche = models.ForeignKey(Branche, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'secteur'
