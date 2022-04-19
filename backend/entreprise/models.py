cdb8044469076100d15ba036e218c8abe195812b
Youcef <chertouhjugurtha@gmail.com>
2022-04-19 12:47:16 +0100

    Added my project

---
+++ backend/entreprise/models.py
@@ -0,0 +1,25 @@
+
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import uuid
+from gerant.models import Gerant
+from commune.models import Commune
+from secteur import Secteur
+# Create your models here.
+class Entreprise(models.Model):
+
+    # Constantes des états de grant:
+
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    raison_social = models.CharField(unique=True, max_length=255, null=False, blank=False)
+    nb_employe_prevus = models.IntegerField(unique=True, validators=[MinValueValidator(1)], null=False)    
+    nb_employe_reel = models.IntegerField(unique=True, validators=[MinValueValidator(1)], null=False)
+    type_entrp = models.CharField(max_length=100, null=False, blank=False)
+    gerant = models.ForeignKey(Gerant, on_delete=models.SET_NULL, null=True)
+    commune = models.ForeignKey(Commune, on_delete=models.SET_NULL, null=True)
+    secteur = models.ForeignKey(Secteur, on_delete=models.SET_NULL, null=True)
+
+
+    class Meta:
+        db_table = 'entreprise'
---
+++ backend/gerant/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import uuid
+
+# Create your models here.
+class Gerant(models.Model):
+
+    # Constantes des états de grant:
+
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    nom = models.CharField(unique=True, max_length=255, null=False, blank=False)
+    prenom = models.CharField(unique=True, max_length=255, null=False, blank=False)
+    age = models.IntegerField(unique=True, validators=[MinValueValidator(1)], null=False)
+    date_naissance = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=100, null=False, blank=False)
+    nationalite = models.CharField(max_length=100,null=False, blank=False)
+    gerant = models.BooleanField(null=False)
+        
+
+    class Meta:
+        db_table = 'grant'
